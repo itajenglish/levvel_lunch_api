affdbce078d2ea32e7576285bbc6b4a7a226524a
Taj English <tajenglish@gmail.com>
2017-11-09 15:50:17 -0500

    Install wrong env package and fixed bug causing server to crash

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotEnv = require('dot-env');
+const env = require('dotenv');
 const logger = require('morgan');
+const app = express();
 
 // Server Config
-dotEnv.load(); // Load ENV Vars
+env.load(); // Load ENV Vars
 app.use(bodyParser.urlencoded({ extended: false })); // Parse Incoming Request Bodies 
 app.use(logger('dev')); // Logs All Network Requests
 app.use(cors()); // Enable Cors For All Network Request
 
 // Server Setup
-const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
