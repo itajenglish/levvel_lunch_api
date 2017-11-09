1f95df2742aeeac8725bf9f0c623498d34007296
Taj English <tajenglish@gmail.com>
2017-11-09 15:42:11 -0500

    Added dot env and made some minor code refactor

--- app.js
+++ app.js
@@ -2,16 +2,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotEnv = require('dot-env');
 const logger = require('morgan');
+
+// Server Config
+dotEnv.load(); // Load ENV Vars
+app.use(bodyParser.urlencoded({ extended: false })); // Parse Incoming Request Bodies 
+app.use(logger('dev')); // Logs All Network Requests
+app.use(cors()); // Enable Cors For All Network Request
 
 // Server Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false })); // body parser
-app.use(logger('dev')) // morgan
-app.use(cors()) //cors
-
 app.listen(PORT, () => {
     console.log(`API Server running on ${PORT}`);
-})
+});
