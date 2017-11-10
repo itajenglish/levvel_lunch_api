66b37d90400866e09ff23c3cb6156408754246fe
Taj English <tajenglish@gmail.com>
2017-11-10 15:46:51 -0500

    Refactored code to es6

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // Deps
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const env = require('dotenv');
-const logger = require('morgan');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import env from 'dotenv';
+import logger from 'morgan';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+
 const app = express();
 
 // Server Config
@@ -11,6 +13,8 @@ env.load(); // Load ENV Vars
 app.use(bodyParser.urlencoded({ extended: false })); // Parse Incoming Request Bodies 
 app.use(logger('dev')); // Logs All Network Requests
 app.use(cors()); // Enable Cors For All Network Request
+app.use('/v1/api', bodyParser.json(), graphqlExpress({ })); // All Requests goes to this endpoint
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/v1/api' })); // Graphql UI(Graphiql)
 
 // Server Setup
 const PORT = process.env.PORT || 3000;
