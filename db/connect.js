c1a8e6b0cdaca7e494f91cd4c5de27c055f49ab8
Taj English <tajenglish@gmail.com>
2017-11-13 16:14:36 -0500

    Updated Connect file with model relationships

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,25 @@
 import Sequelize from 'sequelize';
+import { user, vote, place } from '../models';
 
 const db = new Sequelize('Levvel_Lunch', 'tajenglish', null, {
     dialect: 'postgres'
-})
+});
+
+// Model Relationships
+user.hasMany(vote);
+place.hasMany(vote);
+
+// Database Setup
+db.sync({ force: false })
+    .then(() => {
+        console.log('Database Connection Established!');
+    })
+    .catch(err => {
+        console.log('[Database Error]', err);
+    });
+
+const User = db.models.user;
+const Vote = db.models.vote;
+const Place = db.models.place;
+
+export default { User, Vote, Place };
