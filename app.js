22e072fe73875281d6d9359c5d26c77103320dc9
Taj English <tajenglish@gmail.com>
2018-02-14 13:19:35 -0500

    Removed Graphql and Models from Levvel Lunch Api

--- app.js
+++ app.js
@@ -4,19 +4,14 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import env from 'dotenv';
 import logger from 'morgan';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import Schema from './middlewares/graphql/schema';
-import Resolvers from './middlewares/graphql/resolvers';
 
 const app = express();
 
 // Server Config
 env.load(); // Load ENV Vars
-app.use(bodyParser.urlencoded({ extended: false })); // Parse Incoming Request Bodies 
+app.use(bodyParser.urlencoded({ extended: false })); // Parse Incoming Request Bodies
 app.use(logger('dev')); // Logs All Network Requests
 app.use(cors()); // Enable Cors For All Network Request
-app.use('/v1/api', bodyParser.json(), graphqlExpress({ schema: Schema })); // All Requests goes to this endpoint
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/v1/api', schema: Schema })); // Graphql UI(Graphiql)
 
 // Server Setup
 const PORT = process.env.PORT || 3000;
